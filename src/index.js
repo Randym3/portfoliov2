8e6d8ae1dcaf34eca70f8609955343b7075f5793
randym3 <menesesrandy@gmail.com>
2020-04-15 22:05:47 -0700

    Smaller images, removing css from bundle.js and linking in head tag

--- src/index.js
+++ src/index.js
@@ -2,59 +2,58 @@ import Modal from "./js/modal.js";
 import Project from "./js/projects.js";
 import { elements } from "./js/base.js";
 import "./js/responsivemenu.js";
-import "./css/style.css";
 import { polyfill } from "es6-promise";
 polyfill();
 import "isomorphic-fetch";
 
 class App {
-	constructor() {
-		this.modal = new Modal();
-		this.project = new Project();
-		this.state = {
-			projects: []
-		};
-		this.loadAsynchronous();
-	}
+  constructor() {
+    this.modal = new Modal();
+    this.project = new Project();
+    this.state = {
+      projects: [],
+    };
+    this.loadAsynchronous();
+  }
 
-	loadAsynchronous() {
-		this.project.getProjects().then(data => {
-			this.state.projects = data;
-			this.loadUI();
-		});
-	}
+  loadAsynchronous() {
+    this.project.getProjects().then((data) => {
+      this.state.projects = data;
+      this.loadUI();
+    });
+  }
 
-	loadUI() {
-		const { projectContainer } = elements;
-		const html = this.state.projects.map(cur =>
-			projectMarkUp(
-				cur.imgSrc,
-				cur.title,
-				cur.details,
-				cur.demoURL,
-				cur.gitHubURL,
-				cur.toolsUsed
-			)
-		);
+  loadUI() {
+    const { projectContainer } = elements;
+    const html = this.state.projects.map((cur) =>
+      projectMarkUp(
+        cur.imgSrc,
+        cur.title,
+        cur.details,
+        cur.demoURL,
+        cur.gitHubURL,
+        cur.toolsUsed
+      )
+    );
 
-		for (let i = 0; i < html.length; i++) {
-			projectContainer.innerHTML += html[i];
-		}
+    for (let i = 0; i < html.length; i++) {
+      projectContainer.innerHTML += html[i];
+    }
 
-		let cards = document.querySelectorAll("div.project-card");
-		for (let i = 0; i < cards.length; i++) {
-			cards[i].addEventListener("click", this.modal.openModal);
-		}
+    let cards = document.querySelectorAll("div.project-card");
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", this.modal.openModal);
+    }
 
-		function projectMarkUp(
-			imgSrc,
-			title,
-			details,
-			demoURL,
-			gitHubURL,
-			toolsUsed
-		) {
-			return `
+    function projectMarkUp(
+      imgSrc,
+      title,
+      details,
+      demoURL,
+      gitHubURL,
+      toolsUsed
+    ) {
+      return `
 			<div class="project-card" id="${title.toLowerCase()}" 
 			data-project-title="${title}" 
 			data-project-img-src="${imgSrc}"
@@ -69,20 +68,20 @@ class App {
 					</div>
 				</div>
 			</div>`;
-		}
-	}
+    }
+  }
 
-	registerCloseModalEvents() {
-		const { closeModalButton } = elements;
+  registerCloseModalEvents() {
+    const { closeModalButton } = elements;
 
-		closeModalButton.addEventListener("click", () => this.modal.closeModal());
-		document.addEventListener("click", e => this.modal.outsideClickClose(e));
-	}
+    closeModalButton.addEventListener("click", () => this.modal.closeModal());
+    document.addEventListener("click", (e) => this.modal.outsideClickClose(e));
+  }
 
-	init() {
-		this.registerCloseModalEvents();
-		console.log("Working");
-	}
+  init() {
+    this.registerCloseModalEvents();
+    console.log("Working");
+  }
 }
 
 const app = new App();
